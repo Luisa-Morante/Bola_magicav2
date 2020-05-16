afec40143c54370b4885f0635407478ac9fec615
luisa-morante <luisa.morante@mail.udp.cl>
2020-05-15 21:27:23 -0400

    segundo commit: inicio proyecto 1 app androit programacion 1

--- app/src/main/java/com/example/bola_magica/MainActivity.java
+++ app/src/main/java/com/example/bola_magica/MainActivity.java
@@ -7,13 +7,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView Ball;
     private TextView respuesta;
 
+    private String [] respuestaArray = {"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
+            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
+            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
+            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -28,5 +36,19 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "Buscando tu destino", LENGTH_SHORT).show();
 
     }
+    @Override
+    public void onClick(View v){
 
+
+    switch (v.getId()){
+        case R.id.boton:
+            int randon = new Random().nextInt(respuestaArray.length);
+            respuesta.setText(respuestaArray(randon));
+
+            break;
+
+          }
+    }
 }
+
+
